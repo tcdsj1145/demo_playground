d9b40179abd3704b6a02d168bbd74e0144772215
lumixraku <lumixraku@gmail.com>
2016-01-29 17:52:08 +0800

    change removeads of baidu

--- RemoveAd/rmBdAd.js
+++ RemoveAd/rmBdAd.js
@@ -1,24 +1,37 @@
-myStyle();
-window.addEventListener('load', function() {
-  myStyle();
-  setInterval(removeAD,500);
-}, false);
+// window.addEventListener('DOMContentLoaded', function() {
+//   console.log('dom content load');
 
-function myStyle(){
+// }, false);
+function myStyle() {
   var sheet = document.createElement('style');
-  sheet.innerHTML = ".result a {color:#333 !important;}";
-  document.body.appendChild(sheet);
+  sheet.id = 'myStyle';
+  sheet.innerHTML = "body{background:rgba(242, 242, 242, 0.81)!important;} em{color: #FF4A4A!important;font-size:large!important;} a{color:#333!important;}";
+  document.head.appendChild(sheet);
+  console.log('dom content load----');
 }
 
+var adInterval = setInterval(removeAD, 250);
 function removeAD() {
+  console.log('remove AD');
   var divs = document.querySelectorAll('#content_left>div');
-  divsArr = Array.prototype.slice.call(divs, 0);
-  for (var i = divs.length - 1; i >= 0; i--) {
-    var ele = divs[i];
-    if (ele.className.indexOf('c-container') == -1) {
-      ele.remove();
-      ele.style.cssText = 'background:red';
-      console.log('moved');
+  if(!divs.length){
+    divsArr = Array.prototype.slice.call(divs, 0);
+    for (var i = divs.length - 1; i >= 0; i--) {
+      var ele = divs[i];
+      if (ele.className.indexOf('c-container') == -1) {
+        // ele.style.cssText = 'background:red';
+        ele.remove();
+        console.log('moved');
+      }
     }
-  };
+  }else{
+    clearInterval(adInterval);
+  }
 }
+var styleInterval = setInterval(function() {
+  if (!document.querySelector('#myStyle')) {
+    myStyle();
+  } else {
+    clearInterval(styleInterval);
+  }
+}, 250);
