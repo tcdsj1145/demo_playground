57041c91c008464f8f677e931e546eacd73ffdcf
luonan <luonan@bytedance.com>
2016-06-24 19:23:54 +0800

    add tree2JSON
    Change-Id: I9d2dee3386af87d96df1cfd84d0fb5208bd09fdf

--- Tree/treeEditor.js
+++ Tree/treeEditor.js
@@ -22,7 +22,7 @@
 $(function() {
     $('.tree').click(function(e) {
         var target = e.target;
-        if(target.tagName.toLowerCase() == 'i') return;
+        if (target.tagName.toLowerCase() == 'i') return;
         var $target = $(target);
         var $li = $target.closest('li');
         var $parentsUl = $li.parents('ul');
@@ -30,17 +30,17 @@ $(function() {
         if ($target.hasClass('fa-plus')) {
 
             //层级判断
-            if($parentsUl.length >= 3) {
-                addControls = 0;
-            }
+            // if($parentsUl.length >= 3) {
+            //     addControls = 0;
+            // }
 
             var $ul = $li.children('ul');
             if ($ul && $ul.length) { //有子节点 增加一个
                 $ul.append(_createLi(addControls));
-            } else {   //没有子节点  创建ul再加
+            } else { //没有子节点  创建ul再加
                 $li.append(_createUlWithLi(addControls));
             }
-        }else if($target.hasClass('fa-minus')){
+        } else if ($target.hasClass('fa-minus')) {
             $li.remove();
         }
     });
@@ -48,59 +48,98 @@ $(function() {
 
 /*addControls 必须   控制是否增加action */
 /* text 则是创建有text的元素 */
-function _createUlWithLi(addControls, text){
+function _createUlWithLi(addControls, text) {
     $ul = $('<ul>');
     $ul.append(_createLi(addControls, text));
     return $ul;
 }
-function _createLi(controls, text){
+
+function _createLi(controls, text) {
     text = text || '';
-    var $li = $('<li><div class="text-wrapper"><div class="text"><input type="text" value="'+  text+'"></div></li>');
-    if(controls){
+    var $li = $('<li><div class="text-wrapper"><div class="text"><input type="text" value="' + text + '"></div></li>');
+    if (controls) {
         $li.children('.text-wrapper').append('<div class="action"><span class="fa fa-plus fa-fw"></span><span class="fa fa-minus fa-fw"></span></div>');
     }
     return $li;
 }
 
-function html2json($treeElem){
+//转为嵌套关系的json
+function html2json($treeElem) {
     var tree = {};
     var li = $treeElem.children('ul').children('li');
-    function _htmlToJson(obj, elem){
+    var deep = 0;
+    var deepth = deep;
+
+    function _htmlToJson(obj, elem) {
         var text = elem.find('input').val();
         var $lis = elem.children('ul').children('li');
         obj.text = text;
-        if($lis.length){
+        deep++;
+        if ($lis.length) {
             obj.children = [];
-            $lis.each(function(idx){
+            $lis.each(function(idx) {
                 obj.children.push(_htmlToJson({}, $(this)));
             });
+        } else {
+            deepth = deep > deepth ? deep : deepth;
         }
+        deep--;
         return obj;
     }
-    return _htmlToJson(tree, li);
+    return {
+        tree: _htmlToJson(tree, li),
+        deep: deepth
+    };
 }
 
+
+
+
 //使用id  pid的的一维数组来表现树
-function tree2Array(obj){
+function tree2Array(obj) {
     var array = [];
     var id = 0;
-    function _tree2Array(obj, pid, id){
+
+    function _tree2Array(obj, pid, id) {
         array.push({
             text: obj.text,
             pid: pid,
             id: id
         });
-        if(!obj.children) return;
-        obj.children.forEach(function(item, idx){
-            _tree2Array(item, id, id + '' + (idx+1));
+        if (!obj.children) return;
+        obj.children.forEach(function(item, idx) {
+            _tree2Array(item, id, id + '' + (idx + 1));
         });
     }
-    _tree2Array(obj, 0, 0);
+    _tree2Array(obj.tree, 0, 0);
     console.log(array);
+}
+//使用 id pid sid的一个对象来表现树
+//选择了一个节点之后  通过sid这个值找到所有的子节点
+function tree2Json2(obj) {
+    var output = {};
+    var id = 0;
+
+    function _tree2Array(obj, pid, id) {
+        output[id] = {
+            text: obj.text,
+            pid: pid,
+            id: id,
+            sid: []
+        };
+        if (!obj.children) return;
+        obj.children.forEach(function(item, idx) {
+            _tree2Array(item, id, id + '' + (idx + 1));
+            output[id].sid.push(id + '' + (idx + 1));
+        });
+    }
+    _tree2Array(obj.tree, 0, 0);
+    console.log(output);
 }
 
 
-function json2html(obj){
+
+function json2html(obj) {
     var $tree = $('.tree');
     var $treeUl = $tree.children('ul');
     var layer = 0;
@@ -108,12 +147,12 @@ function json2html(obj){
     $treeUl.html(_json2html(obj));
 
     //创建li
-    function _json2html(obj){
+    function _json2html(obj) {
         layer++;
-        var $li = _createLi(layer == 4 ? 0: 1, obj.text); //加上根节点 最多4层
-        if(obj.children && obj.children.length){
+        var $li = _createLi(layer == 4 ? 0 : 1, obj.text); //加上根节点 最多4层
+        if (obj.children && obj.children.length) {
             var $ul = $('<ul>');
-            obj.children.forEach(function(item){
+            obj.children.forEach(function(item) {
                 $ul.append(_json2html(item));
             });
             $li.append($ul);
@@ -127,8 +166,8 @@ function json2html(obj){
 //{"text":"Root","children":[{"text":"a","children":[{"text":"aa"}]},{"text":"b"},{"text":"c"}]}
 // {"text":"Root","children":[{"text":"a","children":[{"text":"aa"}]},{"text":"b","children":[{"text":"ba","children":[{"text":"baa"}]},{"text":"bb","children":[{"text":"bba"}]}]},{"text":"c","children":[{"text":"ca"}]}]}
 // json2html(JSON.parse('{"text":"Root","children":[{"text":"a","children":[{"text":"aa"}]},{"text":"b"},{"text":"c"}]}'));
-json2html(JSON.parse('{"text":"Root","children":[{"text":"a","children":[{"text":"aa"}]},{"text":"b","children":[{"text":"ba","children":[{"text":"baa"}]},{"text":"bb","children":[{"text":"bba"}]}]},{"text":"c","children":[{"text":"ca"}]}]}'));
+// json2html(JSON.parse('{"text":"Root","children":[{"text":"a","children":[{"text":"aa"}]},{"text":"b","children":[{"text":"ba","children":[{"text":"baa"}]},{"text":"bb","children":[{"text":"bba"}]}]},{"text":"c","children":[{"text":"ca"}]}]}'));
 
-tree2Array(html2json($('.tree')));
+// tree2Array(html2json($('.tree')));
 
-// console.log(html2json($('.tree')));
+console.log(html2json($('.tree')));
